a8b6f37ade0a4cb615ad688d06cdad4864b28c9e
Edgaras Norvilis <edgaras.norvilis@gmail.com>
2025-04-22 23:04:47 +0300

    Console logic update
    Implemented console input handling logic and most edge cases

--- Unity cube app/Assets/Scripts/CommandHandler.cs
+++ Unity cube app/Assets/Scripts/CommandHandler.cs
@@ -8,6 +8,9 @@ public class CommandHandler : MonoBehaviour
     public TMP_Text consoleOutput;
     public ScrollRect console;
 
+    private bool isOnline { get; set; }
+
+
     private void Start()
     {
         inputField.onSubmit.AddListener(HandleInput); // on Submit
@@ -16,21 +19,98 @@ public class CommandHandler : MonoBehaviour
 
     void HandleInput(string input)
     {
-        if (string.IsNullOrWhiteSpace(input)) return;
+        //AddLog(">" + input);
+        string[] command = input.Split(' ');
 
-        // Conditions
-        if (input.ToLower() == "/start")
+        if (isOnline) 
         {
-            AddLog("<color=green>>App is now online!</color>");
+            switch (command[0])
+            {
+                case "/start":
+                    if (command.Length == 1)
+                    {
+                        AddLog("<color=yellow>>App is already online</color>");
+                    }
+                    else
+                    {
+                        AddLog("<color=red>>Invalid command</color>");
+                    }
+                    break;
+                case "/end":
+                    if (command.Length == 1)
+                    {
+                        isOnline = false;
+                        AddLog("<color=red>>App is now offline</color>");
+                    }
+                    else
+                    {
+                        AddLog("<color=red>>Invalid command</color>");
+                    }
+                    break;
+                case "/rotate":
+                    //AddLog(">" + command[0]);
+                    string[] args = command[1].Split(",");
+                    if(args.Length == 3 && float.TryParse(args[0], out _) && float.TryParse(args[1], out _) && float.TryParse(args[2], out _))
+                    {
+                        //Sita desim kazkur
+                        //Vector3 myVector3 = new Vector3(float.Parse(args[0]), float.Parse(args[1]), float.Parse(args[2]));
+                        AddLog(">" + "Zjbs rotatinu xyz");
+                    }
+                    else
+                    {
+                        AddLog("<color=red>>Invalid arguments</color>");
+                    }
+                    break;
+                case "/moveX":
+                    //AddLog(">" + command[0]);
+                    if (command.Length == 2 && float.TryParse(command[1], out _))
+                    {
+                        AddLog(">" + "Zjbs judinam x");
+                        //new Vector3(command[1] * Time.deltaTime, 0, 0);
+                    }
+                    else 
+                    {
+                        AddLog("<color=red>>Invalid arguments</color>");
+                    }
+                    break;
+                case "/echo":
+                    string textLine = "";
+                    for (int i = 1; i < command.Length; i++)
+                    {
+                        textLine += command[i] + " ";
+                    }
+                    AddLog(">" + textLine);
+                    break;
+                default:
+                    AddLog("<color=red>>Invalid command</color>");
+                    break;
+            }
         }
-        else if (input.ToLower() == "/end")
+        else if(command[0] == "/start")
         {
-            AddLog("<color=red>>App is now offline!</color>");
+            if (command.Length == 1)
+            {
+                isOnline = true;
+                AddLog("<color=green>>App is now online</color>");
+            }
+            else
+            {
+                AddLog("<color=red>>Invalid command</color>");
+            }
         }
         else
         {
-            AddLog("> " + input);
+            AddLog("<color=red>>App is offline</color>");
         }
+
+
+
+
+        //AddLog("<color=green>>App is now online</color>");
+        //AddLog("<color=red>>App is now offline</color>");
+        //AddLog("<color=yellow>>App is already online</color>");
+        //AddLog("<color=red>>App is offline</color>");
+
 
         inputField.text = "";
         inputField.ActivateInputField();
